fa44a4d906955c5150234cc2b2a979ad75cbf740
amy.zheng <azheng@fragomen.com>
2023-10-20 00:14:43 +0100

    created card and button shared component and used in employer-portal

--- libraries/shared/ui-components/src/lib/shared-ui-components.module.ts
+++ libraries/shared/ui-components/src/lib/shared-ui-components.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { SharedUiComponentsComponent } from './shared-ui-components.component';
-
-
+import { ButtonComponent } from './button/button.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [
-    SharedUiComponentsComponent
-  ],
-  imports: [
-  ],
-  exports: [
-    SharedUiComponentsComponent
-  ]
+  declarations: [SharedUiComponentsComponent, ButtonComponent, CardComponent],
+  imports: [],
+  exports: [SharedUiComponentsComponent, ButtonComponent, CardComponent],
 })
-export class SharedUiComponentsModule { }
+export class SharedUiComponentsModule {}
--- libraries/shared/ui-components/src/public-api.ts
+++ libraries/shared/ui-components/src/public-api.ts
@@ -5,3 +5,6 @@
 export * from './lib/shared-ui-components.service';
 export * from './lib/shared-ui-components.component';
 export * from './lib/shared-ui-components.module';
+
+export * from './lib/card/card.component'; // Re-export the CardComponent
+export * from './lib/button/button.component'; // Re-export the ButtonComponent
