14c809b8383ea3e428dff2c9045ae758a573bdf2
amy.zheng <azheng@fragomen.com>
2023-10-24 12:28:41 +0100

    configure custom tailwind colour palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./projects/traveller-app/src/**/*.{html, ts}"],
   theme: {
+    /** The colors defined here will completely replace the default color palette  */
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      "navy-blue": "#05445E",
+      "blue-green": "#75E6DA",
+      "baby-blue": "#D4F1F4",
+      "hot-pink": "#FFAEBC",
+      mint: "#B4F8C8",
+      yellow: "#FBE7C6",
+    },
     extend: {},
   },
   plugins: [],
