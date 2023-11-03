869c5f30c1459703ce0d79098be043a2197a3815
amy.zheng <azheng@fragomen.com>
2023-11-03 11:20:24 +0000

    moved header footer into portal dashboard and updated login component to auto initiated Auth0 flow on page load

--- projects/guards-demo/src/app/components/login/login.component.ts
+++ projects/guards-demo/src/app/components/login/login.component.ts
@@ -15,25 +15,13 @@ export class LoginComponent implements OnInit {
   isLoginFailed: boolean = false;
 
   constructor(private router: Router, private authService: AuthService) {}
-  ngOnInit(): void {}
-
-  login(): void {
-    console.log("Try to Login'");
+  ngOnInit(): void {
+    // Automatically redirect to Auth0 Universal Login
     this.authService.loginWithRedirect({ appState: { target: '/portal' } });
-
-    // this.authService.login();
-
-    //Todo:  start OAuth flow for validation
-    // this.authService
-    //   .login({ username: this.username, password: this.password })
-    //   .subscribe((user) => {
-    //     if (user) {
-    //       const { name, email } = user;
-    //       console.log(`name: ${name}, email: ${email}`);
-    //       this.router.navigate(['portal']);
-    //     } else {
-    //       this.isLoginFailed = true;
-    //     }
-    //   });
   }
+
+  // login(): void {
+  //   console.log("Try to Login'");
+  //   this.authService.loginWithRedirect({ appState: { target: '/portal' } });
+  // }
 }
